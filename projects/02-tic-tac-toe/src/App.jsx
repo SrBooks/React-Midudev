9a26f3e638da8c7e5ad1fc761366686da680ecb9
SrBooks <maxpayne046@gmail.com>
2024-02-18 11:56:46 +0100

    Save game to storage and Reset game to storage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -7,14 +7,22 @@ import {Square} from './components/Square.jsx'
 import {TURNS} from './components/constants.js'
 import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import { WinnerModal } from './components/WinnerModal.jsx'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
+
 
 
 
 function App() {
 
-  const [board, setBoard] = useState (Array(9).fill(null))
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
 
-  const [turn, setTurn] = useState (TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   //null is no winner, false is a tie
   const [winner, setWinner] = useState(null)
@@ -23,6 +31,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
+
   }
 
   const updateBoard = (index) => {
@@ -39,6 +50,13 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+    //save game status
+
+    //check if there is a winner
     const newWinner = checkWinnerFrom(newBoard)
     if(newWinner) {
       confetti()
