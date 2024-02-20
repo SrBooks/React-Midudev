1eeb26ff6857b8d7207d24f91a87de1b12af26a4
SrBooks <maxpayne046@gmail.com>
2024-02-20 16:14:48 +0100

    End of mouse follow project

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', {clientX, clientY})
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    //cleaneup of component
+    // --> when the component is disassembled
+    // --> when dependencies change, before executing the effect again
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+
+  }, [enabled])
+
   return (
-    <h3>Proyecto 03</h3>
+    <main>
+      <div className="circle"
+        style={{
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
   )
 }
 
