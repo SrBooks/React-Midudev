19fccce9c359cf78464235b4b2db107545541a5e
SrBooks <maxpayne046@gmail.com>
2024-02-20 19:32:58 +0100

    We leave the circle hidden by default

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -5,16 +5,19 @@ function App() {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
+ // We leave the circle hidden by default
+  const circle = enabled ? 'circle show-circle' : 'circle'
+      
   useEffect(() => {
     const handleMove = (event) => {
       const { clientX, clientY } = event
-      console.log('handleMove', {clientX, clientY})
       setPosition({x: clientX, y: clientY})
     }
 
     if (enabled) {
       window.addEventListener('pointermove', handleMove)
     }
+    console.log(circle)
 
     //cleaneup of component
     // --> when the component is disassembled
@@ -27,9 +30,9 @@ function App() {
 
   return (
     <main>
-      <div className="circle"
+      <div className={circle}
         style={{
-          transform: `translate(${position.x}px, ${position.y}px)`
+          transform: `translate(${position.x}px, ${position.y}px)`,
         }}></div>
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
